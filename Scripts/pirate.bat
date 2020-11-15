ae570e25408d2921a09c9fe4f72fe45340f27ac2
GouravShDev <Gourav0Sharma1@gmail.com>
2020-11-15 17:30:11 +0530

    Added script for searching Torrent

---
+++ Scripts/pirate.bat
@@ -0,0 +1,1 @@
+@pyw D:\Projects\Python\Scripts\pirate.py %*
---
+++ Scripts/pirate.py
@@ -0,0 +1,42 @@
+#! python3
+import requests
+import webbrowser
+import pyperclip
+import sys
+from tkinter import *
+from tkinter import messagebox 
+
+def openBrowser(url):
+    webbrowser.open(url)
+
+def getClipboard():
+    search = pyperclip.paste();
+    return search
+
+def getArgument():
+    if len(sys.argv) > 1:
+        search = ' '.join(sys.argv[1:])
+    else:
+        search = getClipboard()
+    return search
+
+def showErrorDialog():
+    window = Tk()
+    window.eval('tk::PlaceWindow %s center' % window.winfo_toplevel())
+    window.withdraw()
+    
+    messagebox.showerror('Error','No input provided')
+    
+    window.deiconify()
+    window.destroy()
+    window.quit()
+    
+def main():
+    url = "https://1337x.to/search/" + getArgument() + "/1/"
+    if(len(getArgument()) == 0):
+        showErrorDialog()
+        sys.exit()
+    openBrowser(url)
+    
+if __name__ == '__main__':
+    main()
